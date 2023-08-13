bffda053445ff8f4ec1a5c9b0b075b5ecd4d9f90
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-08-13 11:00:27 +0900

    photo型の追加

--- index.js
+++ index.js
@@ -2,26 +2,40 @@ const { ApolloServer } = require('apollo-server')
 
 // MEMO: これはスキーマを定義している
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
 
   type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
   }
 `
 
+var _id = 0
 var photos = []
 
 const resolvers = {
   Query: {
-    totalPhotos: () => photos.length
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
   },
   Mutation: {
     // 第二引数にnameとdescriptionが入っている
     postPhoto(parent, args) {
+      var newPhoto = {
+        id: _id++,
+        ...args
+      }
       photos.push(args)
-      return true
+      return newPhoto
     }
   }
 }
