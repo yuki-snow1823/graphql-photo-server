7843ba98bd8c18c72da2938414f789597af0a9dd
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-08-13 10:50:40 +0900

    mutationの定義をして、resolversを拡張させた

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const { ApolloServer } = require('apollo-server')
 
+// MEMO: これはスキーマを定義している
 const typeDefs = `
   type Query {
     totalPhotos: Int!
   }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Boolean!
+  }
 `
+
+var photos = []
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length
+  },
+  Mutation: {
+    // 第二引数にnameとdescriptionが入っている
+    postPhoto(parent, args) {
+      photos.push(args)
+      return true
+    }
   }
 }
 
